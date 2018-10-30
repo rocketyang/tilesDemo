15ccf01831e821be4e9bd0735d0bc105bd3279f2
raopeng <dell@raopeng.com>
2018-10-30 09:33:51 +0800

    '添加customDefinitionDao'

---
+++ src/main/java/com/example/tiles/CustomDefinitionDao.java
@@ -0,0 +1,23 @@
+package com.example.tiles;
+
+import java.util.Map;
+
+import org.apache.tiles.Definition;
+import org.apache.tiles.definition.dao.DefinitionDAO;
+
+public class CustomDefinitionDao implements DefinitionDAO<String>{
+
+	@Override
+	public Definition getDefinition(String name, String customizationKey) {
+		// TODO Auto-generated method stub
+		
+		return null;
+	}
+
+	@Override
+	public Map<String, Definition> getDefinitions(String customizationKey) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
